b0c07a2ea179de263ce453e8558dc7fedda8223a
Francis Chen <fncis.chen@gmail.com>
2019-03-26 22:28:36 -0700

    added endpoints for server

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const server = express();
 const db = require("./data/dbconfig.js");
 
 server.use(express.json());
+
+server.post("/api/cohorts", (req,res) => {
+
+})
+
+server.get("/api/cohorts/", (req,res) => {
+    
+})
+
+server.get("/api/cohorts/:id", (req,res) => {
+    
+})
+
+server.get("/api/cohorts/:id/students", (req,res) => {
+    
+})
+
+server.put("/api/cohorts/:id", (req,res) => {
+    
+})
+
+server.delete("/api/cohorts/:id", (req,res) => {
+    
+})
